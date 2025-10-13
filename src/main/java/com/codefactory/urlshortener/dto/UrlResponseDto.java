9d16d73c7da970535dd7015f74444c58d492211f
glatorre <glatorre@expedia.com>
2025-10-13 16:08:21 +0200

    update DTOs

--- src/main/java/com/codefactory/urlshortener/dto/UrlResponseDto.java
+++ src/main/java/com/codefactory/urlshortener/dto/UrlResponseDto.java
@@ -1,11 +1,14 @@
 package com.codefactory.urlshortener.dto;
 
-import com.codefactory.urlshortener.entity.Url;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UrlResponseDto {
     private String originalUrl;
     private String shortenedUrl;
